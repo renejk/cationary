6cb085bbe656d2bec9e45bb719fcdfc7f88217e8
renejk <renejosecardonaherrera@gmail.com>
2023-03-08 22:54:00 -0500

    class provider with methods for api and searching

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,23 +1,28 @@
+import 'package:cationary/components/header.dart';
+import 'package:cationary/components/search.dart';
+import 'package:cationary/view_models/home_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Hello World'),
+    HomeViewModel homeViewModel = Provider.of<HomeViewModel>(context);
+    return Scaffold(
+      body: Column(
+        children: [
+          const HeaderWidget(title: 'Cationary'),
+          const SearchWidget(),
+          Expanded(
+            child: ListView(
+              shrinkWrap: true,
+              children: homeViewModel.cats.map((e) => Text(e.name!)).toList(),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
